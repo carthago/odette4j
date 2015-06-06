4e001e7867403d0ea65a6fe2f093da057cc6d4d0
Mike Großmann <mike.grossmann.de@googlemail.com>
2015-06-06 16:50:14 +0200

    import removed

--- src/main/java/com/de/grossmann/carthago/App.java
+++ src/main/java/com/de/grossmann/carthago/App.java
@@ -2,11 +2,9 @@ package com.de.grossmann.carthago;
 
 import com.de.grossmann.carthago.protocol.odette.OFTPClient;
 import com.de.grossmann.carthago.protocol.odette.codec.Transport;
-import com.de.grossmann.carthago.protocol.odette.data.command.SSID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.TimeUnit;
 
 public class App {
 
@@ -23,4 +21,5 @@ public class App {
 
 
     }
+
 }
