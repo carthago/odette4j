04898a96b343e37393dd4a7726c696e57ed4ea28
Mike Großmann <mike.grossmann.de@gmail.com>
2015-10-02 19:59:11 +0200

    some cleanup

--- src/main/java/com/de/grossmann/carthago/protocol/odette/OFTPStateMachine.java
+++ src/main/java/com/de/grossmann/carthago/protocol/odette/OFTPStateMachine.java
@@ -6,34 +6,11 @@ import org.slf4j.LoggerFactory;
 
 import com.de.grossmann.carthago.protocol.odette.data.commands.Command;
 
-import akka.actor.UntypedActor;
-
-public class OFTPStateMachine extends UntypedActor
+public class OFTPStateMachine
 {
     private static final Logger LOGGER;
 
     static {
         LOGGER = LoggerFactory.getLogger(OFTPStateMachine.class);
-    }
-    
-    public void onReceive(Object message) throws Exception {
-      if (message instanceof Command)
-      {
-        LOGGER.info("RCV: " + ((Command) message));
-          SSID ssid = new SSID();
-          ssid.setSsidlev(5L);
-          ssid.setSsidcode("OEVOMIGWIN");
-          ssid.setSsidpswd("111111");
-          ssid.setSsidsdeb(99999L);
-          ssid.setSsidsr("B");
-          ssid.setSsidcmpr("N");
-          ssid.setSsidrest("N");
-          ssid.setSsidspec("N");
-          ssid.setSsidcred(999L);
-          ssid.setSsidauth("N");
-          ssid.setSsidrsv1("");
-          ssid.setSsiduser("");
-          ssid.setSsidcr("\r");
-      }
     }
 }
